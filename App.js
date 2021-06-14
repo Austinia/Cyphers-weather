5eeef4edd0856a91b7716e0da457e77598c4bbfb
Austinia <kimdy90919@gmail.com>
2021-06-14 11:04:00 +0900

    2021/06/14/1103/v1.0.0

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
+import axios from "axios";
+import Weather from './Weather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "6d3552efef3cabbe617fb7ee4d612298";
+const lat = 33.47239800001396;
+const lon = 126.48513646293009;
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+  getWeather = async(lat, lon) => {
+    const { data: {main:{temp},weather} } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+    this.setState({isLoading:false, condition: weather[0].main, temp})
+  }
+  getLocation = async() => {
+    try {
+      // const response= await Location.requestForegroundPermissionsAsync();
+      // console.log(response);
+      // const location = await Location.getCurrentPositionAsync(); //33.47239800001396, 126.48513646293009 네오플
+      // console.log(location);
+      this.getWeather(lat, lon);
+      // send to API and get weather!
+    } catch (error) {
+      Alert.alert("어 버그다!", "제주시에게 알려주기!")
+    }
+
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const { isLoading, temp, condition } = this.state;
+  return isLoading ? <Loading/> : <Weather temp={Math.round(temp)} condition={condition}/>;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Loading.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
+
+export default function Loading(){
+    return <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <Text style={styles.text}>사이퍼즈 서버 날씨 얻어오는 중...</Text>
+    </View>
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // 1 : 모든 공간 사용 가능, 2이상 : 형제들을 가지면 형제flex + 본인flex / 본인flex대로 가져감
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor:"#FDF6AA"
+    },
+    text: {
+        color: "#2c2c2c",
+        fontSize:50
+    }
+})
